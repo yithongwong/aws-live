013f7c179e0a9ce17eb5ef0d3da3f2c6ee30cdb9
yithongwong <wongyithong@gmail.com>
2024-09-19 14:38:07 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.ccpsiiagw0y6.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "wongyithong-employee"
+customregion = "us-east-1"
+
